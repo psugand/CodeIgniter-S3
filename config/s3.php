1211d3bea35e67b81fa1432e1ff9aacc94a59d53
Phil Sturgeon <email@philsturgeon.co.uk>
2010-10-14 12:25:18 +0100

    Library now accepts constructor parameters via the usual $this->load->library('s3', array('foo')) format and will use a config file too.

---
+++ config/s3.php
@@ -0,0 +1,35 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Use SSL
+|--------------------------------------------------------------------------
+|
+| Run this over HTTP or HTTPS. HTTPS (SSL) is more secure but can cause problems
+| on incorrectly configured servers.
+|
+*/
+
+$config['use_ssl'] = TRUE;
+
+/*
+|--------------------------------------------------------------------------
+| Access Key
+|--------------------------------------------------------------------------
+|
+| Your Amazon S3 access key.
+|
+*/
+
+$config['access_key'] = '';
+
+/*
+|--------------------------------------------------------------------------
+| Parser Enabled
+|--------------------------------------------------------------------------
+|
+| Your Amazon S3 Secret Key.
+|
+*/
+
+$config['secret_key'] = '';
